75cf48d834993bedc590dc08d1b001c78ac5d2a8
Silvia Pan <silvia.pan29@gmail.com>
2024-09-20 15:26:47 -0400

    optionally include + sign

--- app/page.tsx
+++ app/page.tsx
@@ -19,20 +19,20 @@ function Ticker({ stockData, handleRemoveTicker }) {
     todayHigh
   )}`;
 
-  function formatIntoCurrency(num: number) {
+  function formatIntoCurrency(num: number, includeSign?: boolean) {
     let formattedNum = num.toLocaleString("en-US", {
       style: "currency",
       currency: "USD",
     });
-    if (num >= 0) {
+    if (num >= 0 && includeSign) {
       formattedNum = `+${formattedNum}`;
     }
     return formattedNum;
   }
 
-  function formatIntoPercentDisplay(num: number) {
+  function formatIntoPercentDisplay(num: number, includeSign?: boolean) {
     let formattedNum = `${num.toFixed(2)}%`;
-    if (num >= 0) {
+    if (num >= 0 && includeSign) {
       formattedNum = `+${formattedNum}`;
     }
     return formattedNum;
@@ -60,7 +60,7 @@ function Ticker({ stockData, handleRemoveTicker }) {
               todayChangePositive ? "has-text-success" : "has-text-danger"
             }`}
           >
-            {formatIntoCurrency(todayChange)}
+            {formatIntoCurrency(todayChange, true)}
           </span>
         </div>
         <div className="level-right">
